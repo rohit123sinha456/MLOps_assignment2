8672ca2f03227e86c8a9bf86bc2dd2614f0df771
Rohit <rohit@gmail.com>
2023-09-26 17:30:05 +0530

    new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+wget https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz
+tar -xzf spark-3.5.0-bin-hadoop3.tgz
+mv spark-3.5.0-bin-hadoop3 spark
+export SPARKHOME=/home/labuser/spark
+export PATH=$PATH:$SPARKHOME/bin
+source ~/.bashrc
+pip install pyspark numpy
