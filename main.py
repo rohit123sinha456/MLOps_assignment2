1d1bad7cf98130bba1e7aaeb2433bca796ea59a6
Rohit Sinha <rohitsinha@gmail.com>
2023-09-27 01:24:25 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -4,14 +4,42 @@ from sklearn.datasets import make_classification
 import pyspark
 from sklearn.metrics import accuracy_score
 
-def distributed_svm_easy(data,w,n_iter):
+def incomplete_cholesky_factorization(matrix):
+    # Computing Cholesky Factorization
+    H = np.linalg.cholesky(matrix)
+    return H
+
+# Function to compute local weights (simplified linear relation)
+def compute_weights(QtQ, lambda_):
+    # Computing Weights
+    w_i = np.dot(QtQ, lambda_)
+    return w_i
+
+def distributed_svm(data_rdd, lambda_):
+    # Map each partition of data to compute H, then QtQ
+    H_rdd = data_rdd.mapPartitions(lambda partition: [incomplete_cholesky_factorization(np.array(list(partition)))])
+    QtQ_rdd = H_rdd.map(lambda H: np.dot(H.T, H))
+    
+    # Collect QtQ from all partitions to the driver
+    QtQ_list = QtQ_rdd.collect()
+    
+    # Compute local weights for each partition
+    local_weights = [compute_weights(QtQ, lambda_) for QtQ in QtQ_list]
+    
+    # Simplified Allreduce to a sum of local weights
+    global_weight = sum(local_weights)
+    
+    return global_weight
+
+# Function to approx weights 
+def LM_weight_approx(data,w,n_iter):
     x = []
     y = []
     l_rate = 0.01
+    # Computing the RDD to list for computing dot products
     for i in data:
         x.append(i[:-1])
         y.append(i[-1])
-
     for i, val in enumerate(x):
         val1 = np.dot(x[i],w)
         if(y[i]*val1 < 1):
@@ -21,27 +49,29 @@ def distributed_svm_easy(data,w,n_iter):
 
     return [w]
 
+# Function to train the model
 def train(sc,data):
     rdd = sc.parallelize(data).cache()
     print(f"Our RDD has {rdd.getNumPartitions()} partitions.")
     w = np.zeros(len(X[0]))
     n_iter = 300
     for iter in range(n_iter):
-        rdd_gradient = rdd.mapPartitions(lambda data : distributed_svm_easy(data,w,n_iter))
+        rdd_gradient = rdd.mapPartitions(lambda data : LM_weight_approx(data,w,n_iter))
         lst = rdd_gradient.collect()
         sumws = 0
         for ws in lst:
             sumws += ws
         w = sumws/len(lst)
     return w
-
+# Function to Test the Model
 def test(x_test,weights):
     y_predicted = []
     for i in x_test:
         y_predicted.append(np.dot(i,weights))
     return y_predicted
 
-def convert_pred_to_binary(y_predicted):
+# Step Function for predicted results
+def step_function(y_predicted):
     y_pred_binary = []
     for i in y_predicted:
         if i<0:
@@ -49,16 +79,19 @@ def convert_pred_to_binary(y_predicted):
         else:
             y_pred_binary.append(1)
     return y_pred_binary
+
+# Main Function to Run the Program
 if __name__=="__main__":
-    X,y = make_classification(n_samples=100000)
-    X_train,X_test = X[:80000],X[80001:]
-    y_train,y_test = y[:80000],y[80001:]
+    X,y = make_classification(n_samples=1000000)
+    X_train,X_test = X[:800000],X[800001:]
+    y_train,y_test = y[:800000],y[800001:]
     sc = pyspark.SparkContext().getOrCreate()
     data = np.hstack([X_train, y_train.reshape(-1,1)]).tolist()
     updated_weights = train(sc,data)
     y_pred = test(X_test,updated_weights)
-    y_pred_bin = convert_pred_to_binary(y_pred)
+    y_pred_bin = step_function(y_pred)
     acc_score = accuracy_score(y_test, y_pred_bin)
-    print(acc_score)
+    print(f" Dataset Size :- 1000000")
+    print(f" Accuracy Score :- {acc_score}")
 
 
